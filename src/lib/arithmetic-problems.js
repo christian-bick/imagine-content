5834dfb9246aaf77bd31d3466758163d89c1dc8c
Christian Bick <christian.bick@bitsuppliers.com>
2025-08-09 12:47:56 +0200

    Split code for better testability

--- src/lib/arithmetic-problems.js
+++ src/lib/arithmetic-problems.js
@@ -8,52 +8,83 @@ const operatorSymbols = {
     divide: '÷'
 };
 
+export function generateSubtraction({minNum1, maxNum1, minNum2, maxNum2}) {
+    const num1 = Math.floor(Math.random() * (maxNum1 - minNum1 + 1)) + minNum1;
+    const maxSubtracted = Math.min(num1, maxNum2)
+    const minSubtracted = Math.max(1, Math.min(maxSubtracted, maxNum2))
+    const num2 = Math.floor(Math.random() * (maxSubtracted - minNum2)) + minNum2;
+    const answer = num1 - num2;
+    return {num1, num2, answer}
+}
+
+export function generateDivision({minNum1, maxNum1, minNum2, maxNum2}) {
+    let divisor, quotient;
+
+    do {
+        divisor = Math.floor(Math.random() * (maxNum2 - minNum2 + 1)) + minNum2;
+        const maxQuotient = Math.floor(maxNum1 / divisor);
+        quotient = Math.floor(Math.random() * maxQuotient);
+    } while (1 >= divisor || 1 >= quotient);
+    return {
+        num1: quotient * divisor,
+        num2: divisor,
+        answer: quotient,
+    }
+}
+
+export function generateMultiplication({minNum1, maxNum1, minNum2, maxNum2}) {
+    const factor1 = Math.floor(Math.random() * (maxNum1 - minNum1 + 1)) + minNum1;
+    const factor2 = Math.floor(Math.random() * (maxNum2 - minNum2 + 1)) + minNum2;
+    return {
+        num1: factor1,
+        num2: factor2,
+        answer: factor1 * factor2,
+    }
+}
+
+export function generateAddition({minNum1, maxNum1, minNum2, maxNum2}) {
+    const num1 = Math.floor(Math.random() * (maxNum1 - minNum1 + 1)) + minNum1;
+    const num2 = Math.floor(Math.random() * (maxNum2 - minNum2 + 1)) + minNum2;
+    return {
+        num1,
+        num2,
+        answer: num1 + num2
+    }
+}
+
 export function generateProblem(op, {maxArg1, maxArg2}) {
     let num1, num2, answer;
-    const maxNum1 = Math.pow(10, maxArg1) - 1;
-    const minNum1 = Math.pow(10, maxArg1 - 1) || 0;
 
-    const maxNum2 = Math.pow(10, maxArg2) - 1;
-    const minNum2 = Math.pow(10, maxArg2 - 1) || 0;
+    const boundaries = {
+        maxNum1: Math.pow(10, maxArg1) - 1,
+        minNum1: Math.pow(10, maxArg1 - 1) || 0,
+
+        maxNum2: Math.pow(10, maxArg2) - 1,
+        minNum2: Math.pow(10, maxArg2 - 1) || 0,
+    }
+
+    let problem = {}
 
     switch (op) {
         case 'subtract':
-            num1 = Math.floor(Math.random() * (maxNum1 - minNum1 + 1)) + minNum1;
-            const maxSubtracted = Math.min(num1, maxNum2)
-            num2 = Math.floor(Math.random() * maxSubtracted) + 1;
-            answer = num1 - num2;
+            problem = generateSubtraction(boundaries)
             break;
 
         case 'multiply':
-            const factor1 = Math.floor(Math.random() * (maxNum1 - minNum1 + 1)) + minNum1;
-            const factor2 = Math.floor(Math.random() * maxNum2) + 1;
-            num1 = factor1;
-            num2 = factor2;
-            answer = num1 * num2;
+            problem = generateMultiplication(boundaries)
             break;
 
         case 'divide':
-            let divisor, quotient;
-
-            do {
-                divisor = Math.floor(Math.random() * (maxNum2 - minNum2 + 1)) + minNum2;
-                const maxQuotient = Math.floor(maxNum1 / divisor);
-                quotient = Math.floor(Math.random() * maxQuotient);
-            } while (1 >= divisor || 1 >= quotient);
-            answer = quotient
-            num1 = quotient * divisor
-            num2 = divisor
+            problem = generateDivision(boundaries)
             break;
 
         case 'add':
         default:
-            num1 = Math.floor(Math.random() * (maxNum1 - minNum1 + 1)) + minNum1;
-            num2 = Math.floor(Math.random() * (maxNum2 - minNum2 + 1)) + minNum2;
-            answer = num1 + num2;
+            problem = generateAddition(boundaries)
             break;
     }
-    const symbol = operatorSymbols[op];
-    return {num1, num2, answer, symbol};
+    problem.symbol = operatorSymbols[op];
+    return problem;
 }
 
 export function generateProblemSet(config) {
