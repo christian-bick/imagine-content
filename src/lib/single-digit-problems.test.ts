a3e6d295109dd37e0965539051bb6c0f7af927fd
Christian Bick <christian.bick@bitsuppliers.com>
2025-08-13 14:34:36 +0200

    Fix tests and bring back old ones

--- src/lib/single-digit-problems.test.ts
+++ src/lib/single-digit-problems.test.ts
@@ -12,6 +12,33 @@ afterEach(() => {
 });
 
 describe('generateAddition', () => {
+    it('should generate a problem with carry and no zero', () => {
+        for (let i = 0; i < 100; i++) {
+            const {num1, num2, answer} = generateAddition(true, false, false);
+            expect(answer).toBeGreaterThanOrEqual(10);
+            expect(num1).not.toBe(0);
+            expect(num2).not.toBe(0);
+            expect(answer).not.toBe(0);
+        }
+    });
+
+    it('should generate a problem with no carry and no zero', () => {
+        for (let i = 0; i < 100; i++) {
+            const {num1, num2, answer} = generateAddition(false, false, false);
+            expect(answer).toBeLessThan(10);
+            expect(num1).not.toBe(0);
+            expect(num2).not.toBe(0);
+        }
+    });
+
+    it('should generate a problem with no carry and with zero', () => {
+        for (let i = 0; i < 100; i++) {
+            const {num1, num2, answer} = generateAddition(false, true, false);
+            expect(answer).toBeLessThan(10);
+            expect(num1 === 0 || num2 === 0 || answer === 0).toBe(true);
+        }
+    });
+
     it('should generate a problem with negative numbers', () => {
         vi.spyOn(Math, 'random')
             .mockReturnValueOnce(0.1) // digit for num1 = 1
@@ -27,17 +54,43 @@ describe('generateAddition', () => {
 });
 
 describe('generateSubtraction', () => {
+    it('should generate a problem with borrow (carry) and no zero', () => {
+        for (let i = 0; i < 100; i++) {
+            const {num1, num2} = generateSubtraction(true, false, true);
+            expect(num1).toBeLessThan(num2);
+            expect(num1).not.toBe(0);
+            expect(num2).not.toBe(0);
+        }
+    });
+
+    it('should generate a problem with no borrow (carry) and no zero', () => {
+        for (let i = 0; i < 100; i++) {
+            const {num1, num2} = generateSubtraction(false, false, false);
+            expect(num1).toBeGreaterThanOrEqual(num2);
+            expect(num1).not.toBe(0);
+            expect(num2).not.toBe(0);
+        }
+    });
+
+    it('should generate a problem with no borrow (carry) and with zero', () => {
+        for (let i = 0; i < 100; i++) {
+            const {num1, num2, answer} = generateSubtraction(false, true, false);
+            expect(num1).toBeGreaterThanOrEqual(num2);
+            expect(num1 === 0 || num2 === 0 || answer === 0).toBe(true);
+        }
+    });
+
     it('should generate a problem with negative numbers', () => {
         vi.spyOn(Math, 'random')
-            .mockReturnValueOnce(0.3) // digit for num1 = 3
-            .mockReturnValueOnce(0.4) // sign for num1 = negative
-            .mockReturnValueOnce(0.5) // digit for num2 = 5
-            .mockReturnValueOnce(0.6); // sign for num2 = positive
+            .mockReturnValueOnce(0.8) // digit for num1 = 8
+            .mockReturnValueOnce(0.6) // sign for num1 = positive
+            .mockReturnValueOnce(0.1) // digit for num2 = 1
+            .mockReturnValueOnce(0.4); // sign for num2 = negative
 
         const {num1, num2, answer} = generateSubtraction(false, false, true);
-        expect(num1).toBe(-3);
-        expect(num2).toBe(5);
-        expect(answer).toBe(-8);
+        expect(num1).toBe(8);
+        expect(num2).toBe(-1);
+        expect(answer).toBe(9);
     });
 });
 
