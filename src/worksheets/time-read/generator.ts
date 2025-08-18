24b2d63591ce43eff75a2719c8ad76a385474e8f
Christian Bick <christian.bick@bitsuppliers.com>
2025-08-18 17:16:31 +0200

    Update generator with new interval scopes

--- src/worksheets/time-read/generator.ts
+++ src/worksheets/time-read/generator.ts
@@ -13,18 +13,27 @@ function generateName(params: { [key: string]: any }) {
     const {interval} = params;
     let intervalName = '';
     if (interval === 3600) intervalName = 'hour';
-    else if (interval === 1800) intervalName = 'half-hour';
-    else if (interval === 900) intervalName = 'quarter-hour';
-    else if (interval === 300) intervalName = 'minute';
-    else if (interval === 6) intervalName = 'second';
+    else if (interval === 1800) intervalName = 'half';
+    else if (interval === 900) intervalName = 'quarter';
+    else if (interval === 60) intervalName = 'minute';
+    else if (interval === 1) intervalName = 'second';
     return `time-read-${intervalName}`;
 }
 
 function generateLabels(params: { [key: string]: any }) {
+    const {interval} = params;
+    let intervalScope;
+    if (interval < 60) {
+        intervalScope = Scope.SecondIntervals
+    } else if (interval < interval < 3600) {
+        intervalScope = Scope.MinuteIntervals
+    } else {
+        intervalScope = Scope.HourIntervals
+    }
     return {
-        Ability: [Ability.MeasurementTime],
-        Scope: [Scope.Time, Scope.Analog, Scope.Digital24Hour],
-        Area: [Area.TimeReading]
+        Area: [Area.MeasuringTime],
+        Ability: [Ability.ProcedureApplication, Ability.ProcedureExecution],
+        Scope: [Scope.AnalogClock, intervalScope],
     }
 }
 
